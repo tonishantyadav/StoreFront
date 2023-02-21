56d0740a665ad7164cc14f53108b6e01ba9559bf
Nishant Yadav <inishantyadav24@gmail.com>
2023-02-21 13:11:06 +0530

    second commit

---
+++ store/urls.py
@@ -0,0 +1,18 @@
+from rest_framework_nested import routers
+from . import views
+
+
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='product')
+router.register('collections', views.CollectionViewset, basename='collection')
+router.register('carts', views.CartViewSet, basename='cart')
+router.register('customers', views.CustomerViewSet, basename='customer')
+router.register('orders', views.OrderViewSet, basename='order')
+
+product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+product_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+cart_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_router.register('items', views.CartItemViewSet, basename='cart-items')
+
+urlpatterns = router.urls + product_router.urls + cart_router.urls
